a6c805a2a88f1666709d456a64d968ae3fe9ac40
Nathalie Seen <n.seen@student.avans.nl>
2021-09-07 15:37:03 +0200

    error if desk doesnt exist

--- index.js
+++ index.js
@@ -1,17 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const deskState = { 1: false, 2: false, 3: true, 4: true, 5: false };
 
-app.get('/', 
-(req, res) => {res.send('Hello World!')})
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 //{deskid: req.params.deskId}
-app.get('/api/desk/:deskId', 
-(req, res) => {res.send({deskid: req.params.deskId})})
-
-
-
+app.get("/api/desk/:deskId", (req, res) => {
+  const deskStateJson = deskState[req.params.deskId];
+  if (typeof deskStateJson === "undefined") {
+    res.status(404).send("deze desk bestaat niet");
+    return;
+  }
+  res.send({
+    deskid: req.params.deskId,
+    deskState:
+      typeof deskStateJson === "undefined" ? "undifined" : deskStateJson,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
