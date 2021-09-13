7dde09887c05661a3bf7ad2f150f61c355d8f260
Nathalie Seen <n.seen@student.avans.nl>
2021-09-13 11:13:31 +0200

    uses body-parser

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 const app = express();
 const port = 3000;
 const deskState = { 1: false, 2: false, 3: true, 4: true, 5: false };
 
 
-app.use(express.static('public'))
+app.use(express.static('public'));
 //{deskid: req.params.deskId}
-
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 app.get("/api/desk/:deskId", (req, res) => {
   const deskStateJson = deskState[req.params.deskId];
   if (typeof deskStateJson === "undefined") {
@@ -20,7 +22,10 @@ app.get("/api/desk/:deskId", (req, res) => {
   });
 });
 app.patch("/api/desk/:deskId", (req, res) => {
-  deskState[req.params.deskId] = !deskState[req.params.deskId];
+  //deskState[req.params.deskId] = !deskState[req.params.deskId];
+  deskState[req.body.deskId] = req.body.deskState;
+  //ik denk dat ik hier dan die deskState van de body van de patch  moet verwerken, maar snap niet hoe
+  console.log(req.body);
   res.send({
     deskid: req.params.deskId,
     deskState: deskState[req.params.deskId],
