cf38e8f0e1799d63cf0c84206621682c2873b2bd
Nathalie Seen <n.seen@student.avans.nl>
2021-11-09 14:34:03 +0100

    added more  desks to db

--- api/src/preDb.ts
+++ api/src/preDb.ts
@@ -21,18 +21,30 @@ export async function preDb() {
   await db.exec(
     "CREATE TABLE IF NOT EXISTS user (name TEXT PRIMARY KEY, password TEXT)"
   );
-  await db.exec("CREATE TABLE IF NOT EXISTS desk (desk_id TEXT PRIMARY KEY, deskstate TEXT)");
-    await db.exec( "CREATE TABLE IF NOT EXISTS booking (booking_id TEXT PRIMARY KEY, start_time TEXT, end_time TEXT, date TEXT)");
+  await db.exec(
+    "CREATE TABLE IF NOT EXISTS desk (desk_id TEXT PRIMARY KEY, deskstate TEXT)"
+  );
+  await db.exec(
+    "CREATE TABLE IF NOT EXISTS booking (booking_id TEXT PRIMARY KEY, start_time TEXT, end_time TEXT, date TEXT)"
+  );
 
-
-  interface User{name:string; password:string}
-  const allUsers = await db.all<User[]>("SELECT * FROM user WHERE name = (?)", "test" );
+  interface User {
+    name: string;
+    password: string;
+  }
+  const allUsers = await db.all<User[]>(
+    "SELECT * FROM user WHERE name = (?)",
+    "test"
+  );
   console.log(allUsers);
-  if(allUsers.length === 0){
+  if (allUsers.length === 0) {
     await db.exec('INSERT INTO user VALUES ("test", "pw")');
-    await db.exec('INSERT INTO desk VALUES ("b2.1", "free")');
-    await db.exec('INSERT INTO desk VALUES ("b2.2", "free")');
-    await db.exec( 'INSERT INTO booking VALUES ("bookingb2.1.1", "13:00", "17:00", "4-11-2021")');
+    for (let i = 1; i < 12; i++) {
+      await db.run('INSERT INTO desk VALUES ((?), "free")', `b2.${i}`);
+    }
+    await db.exec(
+      'INSERT INTO booking VALUES ("bookingb2.1.1", "13:00", "17:00", "4-11-2021")'
+    );
   }
 }
 export default preDb;
