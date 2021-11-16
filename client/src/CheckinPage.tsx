0c1605863dade87b23c179f3832b49012c0c6a11
Nathalie Seen <n.seen@student.avans.nl>
2021-11-16 14:38:44 +0100

    added get method checkin

--- client/src/CheckinPage.tsx
+++ client/src/CheckinPage.tsx
@@ -4,10 +4,14 @@ import React, { useState } from 'react';
 function Checkin_page() {
     const deskId = document.location.search.substr(1);
     const [currentDeskState, setCurrentDeskState] = useState('free');
+    fetch(`http://localhost:3001/api/desk/${deskId}`)
+    .then((data) => data.json())
+    .then((data) =>{
+      setCurrentDeskState(data.deskState);
+    })
   
     const handleToggleChecked = ()=>{
       fetch(`http://localhost:3001/api/desk/${deskId}`,{
-        //can also be a PUT method
         method: 'PATCH',
         headers: {
           "Content-type": "application/json; charset=UTF-8"
@@ -26,7 +30,11 @@ function Checkin_page() {
     return (
       <div className="CheckinPage">
         <header className="checkinHeader">
-          <Button variant = "contained" color="secondary" onClick={handleToggleChecked}>check in/ uit</Button>
+          <Button variant = "contained" color="secondary" 
+          disabled={currentDeskState === 'unavailable'}
+          onClick={handleToggleChecked}
+          >check in/ uit</Button>
+          
           {currentDeskState === 'free' ? 'uitgechecked' : 'ingechecked' }
         </header>
       </div>
