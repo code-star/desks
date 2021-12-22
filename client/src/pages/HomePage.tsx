d02700626e8c26d7e5d21159f0ecd52152fada57
mdvanes <es@mdworld.nl>
2021-12-22 08:45:18 +0100

    Add desk selector

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,16 +1,16 @@
-import { Toolbar, Drawer, Divider, List, ListItem, ListItemText, ListItemButton, Card, CardContent, Typography, CardHeader, Box, CardActions, Button, LinearProgress, Alert } from "@mui/material";
+import { Toolbar, Drawer, Divider, List, ListItem, ListItemText, ListItemButton, Card, CardContent, Typography, CardHeader, Box, CardActions, Button, LinearProgress, Alert, InputAdornment, TextField } from "@mui/material";
 import { FC, useEffect, useState } from "react";
 
 const DRAWER_WIDTH = 240;
 const BOOKING_ROUTE_URL = "/desks/book";
 const BOOKING_LABEL = "Make a new booking";
-const CHECKIN_ROUTE_URL = "/desks/checkin/?b2.1";
-const CHECKIN_LABEL = "Check in at desk b2.1";
-
+const CHECKIN_ROUTE_URL = "/desks/checkin/?b2.";
+const CHECKIN_LABEL = "Check in at desk b2.";
 
 const HomePage: FC = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [hasError, setHasError] = useState(false);
+    const [deskNr, setDeskNr] = useState<number>(1);
 
     const getDesks = async () => {
         setIsLoading(true);
@@ -33,6 +33,15 @@ const HomePage: FC = () => {
         getDesks();
     }, []);
 
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const newDeskNr = parseInt(event.target.value, 10);
+        if(!isNaN(newDeskNr)) {
+            setDeskNr(newDeskNr);
+        } else {
+            console.error("Error: not a number, might be an intermediate value");
+        }
+    }
+
     return <Box sx={{ display: "flex" }}>
         <Drawer variant="permanent" anchor="left" sx={{
             width: DRAWER_WIDTH,
@@ -51,8 +60,8 @@ const HomePage: FC = () => {
                     </ListItemButton>
                 </ListItem>
                 <ListItem>
-                    <ListItemButton component="a" href={CHECKIN_ROUTE_URL}>
-                        <ListItemText primary={CHECKIN_LABEL} />
+                    <ListItemButton component="a" href={`${CHECKIN_ROUTE_URL}${deskNr}`}>
+                        <ListItemText primary={`${CHECKIN_LABEL}${deskNr}`} />
                     </ListItemButton>
                 </ListItem>
             </List>}
@@ -67,11 +76,22 @@ const HomePage: FC = () => {
                     <Typography variant="h6" gutterBottom>Booking a desk</Typography>
                     <Typography gutterBottom>Here you can book a desk for you and your team. Keep in mind ...</Typography>
                     <Typography variant="h6" gutterBottom>Checking in</Typography>
-                    <Typography>To check in ...</Typography>
+                    <Typography>To check in you can scan the QR code on the desk or type in the desk number below. Keep in mind ...</Typography>
+                    <TextField
+                        label="Desk number"
+                        id="outlined-start-adornment"
+                        sx={{ m: 1, width: '25ch' }}
+                        InputProps={{
+                            startAdornment: <InputAdornment position="start">B2.</InputAdornment>,
+                        }}
+                        value={deskNr}
+                        onChange={handleChange}
+                        type="number"
+                    />
                 </CardContent>
                 {!hasError && <CardActions>
                     <Button component="a" href={BOOKING_ROUTE_URL}>{BOOKING_LABEL}</Button>
-                    <Button component="a" href={CHECKIN_ROUTE_URL} color="secondary">{CHECKIN_LABEL}</Button>
+                    <Button component="a" href={`${CHECKIN_ROUTE_URL}${deskNr}`} color="secondary">{`${CHECKIN_LABEL}${deskNr}`}</Button>
                 </CardActions>}
             </Card>
         </Box>
