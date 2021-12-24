994bb53d94c472cb9dfba743d8177702d1bd9517
mdvanes <es@mdworld.nl>
2021-12-24 16:42:24 +0100

    Fix routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,25 @@
-import { ThemeProvider} from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material/styles';
 import BookingPage from './pages/BookingPage';
 import CheckinPage from './pages/CheckinPage';
 import { TopBar } from './components/TopBar';
-import { Route } from 'react-router-dom'
+import { Route, BrowserRouter, Switch } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import { CssBaseline } from '@mui/material';
-import {theme} from './theme'
+import { theme } from './theme'
 
 function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <TopBar />
-        <Route path="/desks/book" component={BookingPage} />
-        <Route path="/desks/checkin" component={CheckinPage} />
-        <Route exact path="/" component={HomePage} />
+        <BrowserRouter>
+          <CssBaseline />
+          <TopBar />
+          <Switch>
+            <Route path="/desks/book" component={BookingPage} />
+            <Route path="/desks/checkin" component={CheckinPage} />
+            <Route component={HomePage} />
+          </Switch>
+        </BrowserRouter>
       </ThemeProvider>
     </div>
   );
