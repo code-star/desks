5db397aade6a3e42ac50ddcdcab5540d66692c7e
Nathalie Seen <n.seen@student.avans.nl>
2022-01-20 19:30:58 +0100

    changes userpage, get list for notifications

--- api/src/routes/notifcation.ts
+++ api/src/routes/notifcation.ts
@@ -3,40 +3,36 @@ import sqlite3 from "sqlite3";
 import { Booking } from "../types";
 
 const UNIX_HALF_HOUR = 1800;
-//calls function every 30 seconds
+//calls function every 5 seconds
 export function intervalId(
   db: Database<sqlite3.Database, sqlite3.Statement>
 ): void {
   setInterval(async () => {
     const bookings = await db.all<Booking[]>("SELECT * FROM booking");
-    const bookingNow: Booking | undefined = bookings.find(
-      (booking: Booking) => {
-        return isBetween(
-          booking.start_time,
-          Date.now() / 1000 - 3 + UNIX_HALF_HOUR,
-          Date.now() / 1000 + 3 + UNIX_HALF_HOUR
-        );
-      }
-    );
-    const bookingOver: Booking | undefined = bookings.find(
-      (booking: Booking) => {
-        return isBetween(
-          booking.start_time,
-          Date.now() / 1000 - 3 - UNIX_HALF_HOUR,
-          Date.now() / 1000 + 3 - UNIX_HALF_HOUR
-        );
-      }
-    );
-    if (bookingNow) {
+    const bookingsNow: Booking[] = bookings.filter((booking: Booking) => {
+      return isBetween(
+        booking.start_time,
+        Date.now() / 1000 - 3 + UNIX_HALF_HOUR,
+        Date.now() / 1000 + 3 + UNIX_HALF_HOUR
+      );
+    });
+    const bookingsOver: Booking[] = bookings.filter((booking: Booking) => {
+      return isBetween(
+        booking.start_time,
+        Date.now() / 1000 - 3 - UNIX_HALF_HOUR,
+        Date.now() / 1000 + 3 - UNIX_HALF_HOUR
+      );
+    });
+    bookingsNow.map((bookingNow) => {
       console.log(`half hour before booking desk ${bookingNow.booked_desk}`);
       //sign frontend 'Half an our before your booking of desk x'
-    }
-    if (bookingOver) {
+    });
+    bookingsOver.map((bookingOver) => {
       console.log(
         `you are too late for your booking of ${bookingOver.booked_desk}`
       );
       //sign frontend
-    }
+    });
   }, 5000);
 }
 
